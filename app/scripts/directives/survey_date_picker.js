5165fb18bc3aaeccf2c9c30d6cfb54af5a81d2ef
Simon Yu <simonykq@gmail.com>
2014-12-30 08:36:01 +0000

    delete post scripts.  edit survive date picker directive

--- app/scripts/directives/survey_date_picker.js
+++ app/scripts/directives/survey_date_picker.js
@@ -7,7 +7,7 @@
  * # surveyDatePicker
  */
 angular.module('surveyApp')
-  .directive('surveyDatePicker', function ($filter) {
+  .directive('surveyDatePicker', function ($filter, $parse) {
     return {
       templateUrl: 'views/_survey_date_picker.html',
       replace: true,
@@ -31,26 +31,59 @@ angular.module('surveyApp')
       link: function postLink(scope, element, attrs, ctrl) {
         if(!ctrl) return;
 
-        var ngModelCtrl = ctrl;
+        var ngModelCtrl = ctrl,
+            format      = scope.format || 'shortDate',
+            parsed      = $parse('dateValue'),
+            setter      = parsed.assign;
 
-        ngModelCtrl.$render = function(){
-           element.find('input').text(ngModelCtrl.$viewValue);
-        };
-
-        var dateFormatter = function(value){
-           var format = scope.format || 'shortDate';
-           return $filter('date')(format, value)
-        };
-
-        ngModelCtrl.$formatters.push(dateFormatter);
-
-
-        scope.$watch('opened',function(newValue){
-          var ele = element.lastChild();
-          newValue ? ele.show() : ele.hide();
-
+        scope.$watch(function(){
+          return ngModelCtrl.$viewValue;
+        },function(newVale){
+          setter(scope, newVale);
+        });
+        scope.$watch('dateValue', function(newValue){
+          ngModelCtrl.$setViewValue(newValue);
         });
 
+        ngModelCtrl.$render = function(){
+           var formatedDate = $filter('date')(format,ngModelCtrl.$viewValue);
+           element.find('input').text(formatedDate);
+        };
+
+        attrs.$observe('format', function(value){
+          format = value || 'shortDate';
+          ngModelCtrl.$render();
+        });
+
+        // popup element used to display calendar
+        var popupEl = angular.element('<div id="calendar-popup" datepicker-popup-wrap><div datepicker></div></div>'),
+            datepickerEl = angular.element(popupEl.children()[0]);
+
+        popupEl.attr({
+          'ng-model': 'dateValue'
+        });
+
+        angular.forEach(scope.options,function(value, option){
+          datepickerEl.attr( cameltoDash(option), value );
+        });
+
+        scope.$watch('opened',function(newValue){
+          var ele = element.find('#calendar-popup');
+          newValue ? ele.show() : ele.hide();
+        });
+
+        var $popup = $compile(popupEl)(scope);
+        // Prevent jQuery cache memory leak (template is now redundant after linking)
+        popupEl.remove();
+        element.after($popup);
+
+        scope.$on('$destroy', function(){
+          $popup.remove();
+        });
+
+        function cameltoDash( string ){
+          return string.replace(/([A-Z])/g, function($1) { return '-' + $1.toLowerCase(); });
+        }
       }
     };
   });
